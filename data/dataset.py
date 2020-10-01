7bedb41c85be2866057fca0ab7403abd4b31f443
Hexmagic <191440042@qq.com>
2020-10-01 18:10:37 +0800

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -118,7 +118,7 @@ class VOCDataset(torch.utils.data.Dataset):
         w,h = image.size
         boxes[:, 0::2] = np.clip(boxes[:, 0::2] / w, 0.001, 0.999)
         boxes[:, 1::2] = np.clip(boxes[:, 1::2] / h, 0.001, 0.999)
-        image = cv2.resize(image, (416, 416))
+        image =image.resize((416, 416))
         return ToTensor(image), boxes, labels, len(boxes)
 
     def get_annotation(self, index):
